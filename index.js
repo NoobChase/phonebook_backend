9ad030e05120d4a21f1f0cb7d4e9cbef6cdc8ba4
unknown <chasereynolds2019@gmail.com>
2022-09-16 09:09:26 -0500

    adding validation

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get('/info',(request, response) => {
   
 })
 
-app.post('/api/persons',(request,response)=>{
+app.post('/api/persons',(request,response,next)=>{
   const body = request.body
   console.log(body)
   if (!body.name) {
@@ -96,6 +96,7 @@ app.post('/api/persons',(request,response)=>{
   person.save().then(savedPerson =>{
     response.json(savedPerson)
   })
+  .catch(error => next (error))
 })
 /*
 app.post('/api/persons',(request, response) => {
@@ -174,6 +175,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
+  else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 const password = process.argv[2]
 
 const url = process.env.MONGODB_URL
@@ -12,8 +13,19 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minlength: 3
+  },
   number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
